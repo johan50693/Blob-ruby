b817b2f5e2ccda79b2a8346996592388c89ddf48
Albert <johan50693@gmail.com>
2017-04-06 22:25:29 -0400

    strong params, vistas parciales y autenticacion de usuarios

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,8 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new( title: params[:article][:title], body: params[:article][:body])
+		#@article = Article.new( title: params[:article][:title], body: params[:article][:body])
+		@article = Article.new( article_params)
 
 		if @article.save
 
@@ -23,10 +24,30 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def edit
+		@article= Article.find(params[:id])
+	end
+
+	def update
+		@article= Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
+
+	end
+
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
 
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title,:body)
 	end
 end
